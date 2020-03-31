ccc86374410a6f644d04a53a0bf09db0e78f29f1
Grace <graciousdaze@gmail.com>
2020-03-31 15:37:15 -0500

    add peek() and remove()

--- src/ArrayQueue.java
+++ src/ArrayQueue.java
@@ -1,3 +1,5 @@
+import java.util.NoSuchElementException;
+
 /**
  * This class implements the Queue data structure by using circular arrays
  * to store and manipulate objects.
@@ -26,6 +28,85 @@ public class ArrayQueue {
 		head = 0;
 		rear = 0;
 		numItems = 0;
+	}
+	
+	/**
+	 * Returns if the queue is empty or not
+	 * 
+	 * @return True The queue is empty
+	 * 		   False The queue is not empty
+	 */
+	public boolean isEmpty()
+	{
+		if(numItems == 0)
+			return true;
+		
+		return false;
+	}
+	
+	/**
+	 * Returns a copy of the element stored at the front of the queue
+	 * 
+	 * @return Object copy of the first item in the queue
+	 */
+	public Object peek()
+	{
+		try
+		{
+			if(numItems == 0)
+				throw new NoSuchElementException();
+			
+			Object copy = contents[head];
+			
+			return copy;
+		}
+		catch(NoSuchElementException e)
+		{
+			System.out.println("The queue is empty");
+			return null;
+		}
+	}
+	
+	/**
+	 * Removes and returns the object that is currently at the front of 
+	 * the queue
+	 * 
+	 * @return The object at the front of the queue
+	 */
+	
+	public Object remove()
+	{
+		try 
+		{
+			//If numItems is 0 throw an exception
+			if(numItems == 0)
+				return new NoSuchElementException();
+			
+			//Store the top item in a variable and set current head index
+			//to null
+			Object removed = contents[head];
+			contents[head] = null;
+			
+			//If head position is equal to last index value, head == 0
+			if(head == contents.length - 1)
+			{
+				head = 0;
+			}
+			else
+			{
+				//Otherwise increment head position by 1
+				head = head++;
+			}
+			
+			//Return removed object
+			return removed;
+		}
+		catch(NoSuchElementException e)
+		{
+			//Print error and return null
+			System.out.println("The queue is empty");
+			return null;
+		}
 	}
 	
 }
